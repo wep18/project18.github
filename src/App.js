34c868d12cdf4271df0fec2713e15870180d19f9
ayham <s12011081@stu.najah.edu>
2023-12-20 13:56:42 +0200

    sign up and download

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import FirstPhoto from './component/FirstPhoto';
+
+function App() {
+  return (
+    <div className="App">
+    <FirstPhoto/>
+
+    </div>
+  );
+}
+
+export default App;
